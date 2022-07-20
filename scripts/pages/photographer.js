fbc4a8456859a54256b4ccaf9c7cede0d7d1e424
Brice Meyer <hi.meyerb@gmail.com>
2022-07-20 15:25:29 +0200

    CSS edited for the media view

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -11,14 +11,15 @@ new dataApi()
             const photographerCard = document.createElement('article');
             photographerCard.className += 'photographer-container';
             let htmlTemplate = `
-            <div>
-                <img src='../../assets/photographers/${photographer.portrait}' alt='Portrait de ${photographer.name}'>
-                <div class="photographer-info">
+            <div class="photographer-info">
+                <div class="photographer-text-info">
                     <h2 class="photographer_name">${photographer.name}</h2>
                     <h3 class="city">${photographer.city}</h3>
                     <h5 class="tagline">${photographer.tagline}</h5>
                     <!-- <h4 class="price">${photographer.price}€/jour</h4> -->
                 </div>
+                <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+                <img src='../../assets/photographers/${photographer.portrait}' alt='Portrait de ${photographer.name}'>
             </div>
             `;
             photographerContainer.appendChild(photographerCard);
@@ -36,7 +37,7 @@ new dataApi()
                     <img src='../../assets/images/${media.image}' alt='${media.title}'>
                     <div class='media-info'>
                         <p>${media.title}</p>
-                        <p>${media.likes}</p>
+                        <p>${media.likes} <i class="fa-solid fa-heart"></i></p>
                     </div>
                 `;
                 mediaContainer.appendChild(mediaCard);
@@ -47,7 +48,7 @@ new dataApi()
                     <video controls src='../../assets/images/${media.video}' type="video/mp4"></video>
                     <div class='media-info'>
                         <p>${media.title}</p>
-                        <p>${media.likes}</p>
+                        <p>${media.likes} <i class="fa-solid fa-heart"></i></p>
                     </div>
                 `;
                 mediaContainer.appendChild(mediaCard);
