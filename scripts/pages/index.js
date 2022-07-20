4cf3d7e49b2d00b8a482019209142e31d3a2ca29
Brice Meyer <hi.meyerb@gmail.com>
2022-07-20 12:17:43 +0200

    Get the right photographer on photographer page + begining of media display

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -30,7 +30,7 @@ new dataApi()
     photographerCard.innerHTML += htmlTemplate;
   })
 }) 
-  async function getPhotographers() {
+  //async function getPhotographers() {
     // const response = await fetch("data/photographers.json");
     // const data = await response.json();
     // const photographers = [{
@@ -49,22 +49,22 @@ new dataApi()
     // return ({
     //   photographers: [...photographers]
     // })
-}
+//}
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+// async function displayData(photographers) {
+//   const photographersSection = document.querySelector(".photographer_section");
 
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
-}
+//   photographers.forEach((photographer) => {
+//     const photographerModel = photographerFactory(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+//     photographersSection.appendChild(userCardDOM);
+//   });
+// }
 
-async function init() {
+//async function init() {
   // Récupère les datas des photographes
   //const { photographers } = await getPhotographers();
   //displayData(photographers);
-}
+//}
 
 //init();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,39 @@
-//Mettre le code JavaScript lié à la page photographer.html
+let getURL = new URLSearchParams(window.location.search);
+let getId = getURL.get("id");
+let photographerId = parseInt(getId);
+
+import dataApi from "../utils/fetchdata.js";
+new dataApi()
+.getData().then((data) => {
+    data.photographer.map((photographer) => {
+        if(photographer.id == photographerId) {
+            const photographerContainer = document.querySelector('.photograph-header');
+            const photographerCard = document.createElement('article');
+            photographerCard.className += 'photographer-container';
+            let htmlTemplate = `
+            <div>
+                <img src='../../assets/photographers/${photographer.portrait}' alt='Portrait de ${photographer.name}'>
+                <div class="photographer-info">
+                    <h2 class="photographer_name">${photographer.name}</h2>
+                    <h3 class="city">${photographer.city}</h3>
+                    <h5 class="tagline">${photographer.tagline}</h5>
+                    <!-- <h4 class="price">${photographer.price}€/jour</h4> -->
+                </div>
+            </div>
+            `;
+            photographerContainer.appendChild(photographerCard);
+            photographerCard.innerHTML += htmlTemplate;
+            console.log("L'id du photographe est " + photographer.id);
+        }
+    })
+    data.media.map((media) => {
+        if(media.photographerId == photographerId) {
+            console.log("Id photo du photographe : " + media.id);
+            let htmlTemplate = `
+            <div>
+                <img src='../../assets/images/${media.image}' alt='${media.title}'>
+            </div>
+            `;
+        }
+    })
+}) 
