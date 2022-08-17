23d5b9020087af05b248d45a11d4d08183a23ec9
Brice Meyer <hi.meyerb@gmail.com>
2022-08-17 19:01:58 +0200

    Changing dialog index and clearing elements

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -2,12 +2,16 @@
 function openModalPic() {
     const mediaId = document.querySelectorAll('.media-container article');
     for (let i = 0; i <= mediaId.length - 1; i++) {
+        //const imgMedia = document.querySelectorAll('.media-container article img');
         mediaId[i].addEventListener("click", (event) => {
             event.preventDefault();
+            //Getting all the info from the targeted article
             let i = event.currentTarget.getAttribute("data-index");
             let mediaSrc = event.target.getAttribute("src");
             let mediaAlt = event.target.getAttribute("alt");
+            
             const mediaContainer = document.querySelector('.media-container');
+            //Creating the dialog box to show targeted article elements
             const mediaCard = document.createElement('dialog');
             mediaCard.id = "myModal";
             mediaCard.className = "img-modal";
@@ -16,10 +20,14 @@ function openModalPic() {
             console.log(mediaSrc);
             let modalTemplate = `
                 <span class="close cursor" onClick="closeModal();">&times;</span>
-                <div class="modal-content">
+                <div class="modal-content" data-index="${i}">
                     <div class="mySlides">
                         <img class='imgModal' src='${mediaSrc}' alt='${mediaAlt}'>
-                        <p class='caption-container'>${mediaAlt}</p>
+                    </div>
+                    <span class='prev' onClick='prevMedia();'>&#10094;</span>
+                    <span class='next' onClick='nextMedia();'>&#10095;</span>
+                    <div class='caption-container'>
+                        <p class='caption'>${mediaAlt}</p>
                     </div>
                 </div>
             `;
@@ -34,6 +42,28 @@ function openModalPic() {
     //         modal.open = true;
     //     })
     // }
+}
+
+function prevMedia() {
+    const targetModal = document.getElementById('myModal');
+    const targetDiv = targetModal.querySelector('.modal-content');
+    const getMediaIndex = targetDiv.getAttribute('data-index');
+    console.log("Prev data-index is " + (getMediaIndex - 1));
+
+    //Delete Current img and caption opened on the dialog box
+    const slide = targetDiv.querySelector('.imgModal');
+    const captionSlide = targetDiv.querySelector('.caption');
+    slide.parentElement.removeChild(slide);
+    captionSlide.parentElement.removeChild(captionSlide);
+    //Changing data-index
+    targetDiv.setAttribute('data-index', (getMediaIndex - 1));
+}
+
+function nextMedia() {
+    const targetModal = document.getElementById('myModal');
+    const targetDiv = targetModal.querySelector('.modal-content');
+    const getMediaIndex = targetDiv.getAttribute('data-index');
+    console.log("Next data-index is " + (getMediaIndex + 1));
 }
 
 // Close the Modal
